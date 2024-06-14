6fe4e6f038bdc82fcd57621d403f3c2aec37cdb4
Medet Ahmetson <milayter@gmail.com>
2024-06-14 22:37:46 +0700

    Set Sepolia Frogwifcat

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -3,9 +3,9 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { stakingABI } from "./stakingABI";
 
-const nftContractAddress = "<contract_address>";
-const rewardTokenContractAddress = "<contract_address>";
-const stakingContractAddress = "<contract_address>";
+const nftContractAddress = "0x6eA56F469f5337d7A78645a1C1e103BF8975B0c7";
+const rewardTokenContractAddress = "0x32ce985bCab4961394A9167D15F5d509D6F23f06";
+const stakingContractAddress = "0x02B3e8fe0Fa3a27297e258663D71713eB859F061";
 
 export const NFT_CONTRACT = getContract({
     client: client,
